afcaec4b3b9011ae7973cb790e9f520b5880f552
cjq002 <chenjiaqing@Mac-mini-CJQ.local>
2015-02-09 08:35:41 +0800

    Initial Commit

---
+++ UITextView的创建与使用Tests/UITextView______Tests.m
@@ -0,0 +1,34 @@
+//
+//  UITextView______Tests.m
+//  UITextView的创建与使用Tests
+//
+//  Created by 陈家庆 on 15-2-9.
+//  Copyright (c) 2015年 shikee_Chan. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface UITextView______Tests : XCTestCase
+
+@end
+
+@implementation UITextView______Tests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
